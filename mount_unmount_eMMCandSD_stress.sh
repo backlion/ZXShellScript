a8efc2e9788253d93d3fea08df28dc20b309111b
lin199231 <lin199231@gmail.com>
2015-01-12 13:25:46 +0800

    [UPADTE]change mount_unmount_stress
    修改mount_unmount_stress来兼容83.00.07的自动mount数据卡及U盘

--- mount_unmount_eMMCandSD_stress.sh
+++ mount_unmount_eMMCandSD_stress.sh
@@ -1,29 +1,34 @@
 #Author：Issac Lin
 #Ext: 7536
-#version:1.0
+#version:1.1
 #mount_unmount_eMMCandSD_stress
 #log_location:./mount_unmount_eMMCandSD_stress.log
 #
-echo "mount_unmount_eMMCandSD_stress version: 1.0"
+echo "mount_unmount_eMMCandSD_stress version: 1.1"
 if test -z $1
 then
 	echo "You must input the mount and unmount times"
 else
+	rm mount_unmount_eMMCandSD_stress.log
 	LOOP=1
 #	mkdir /data/fat
 	mount -o remount,rw /
 	mkdir /temp
-	umount /data/fat
+	umount /mnt/media_rw/extsdcard
 	date >> mount_unmount_eMMCandSD_stress.log
 	while test LOOP -le $1
 	do
-		mount -t vfat /dev/block/mmcblk0p1  /data/fat/
+		mount -t vfat /dev/block/mmcblk0p1  /mnt/media_rw/extsdcard
 		echo "mount $LOOP times"
 		echo "mount $LOOP times" >> mount_unmount_eMMCandSD_stress.log
-		umount /data/fat
+		umount /mnt/media_rw/extsdcard
 		echo "unmount $LOOP times"
 		echo "unmount $LOOP times" >> mount_unmount_eMMCandSD_stress.log
 		let LOOP++
 	done
-	sh via_cpcmp test /temp
+	mount -t vfat /dev/block/mmcblk0p1  /mnt/media_rw/extsdcard
+	mkdir /mnt/media_rw/extsdcard/temp
+	sh via_cpcmp test /mnt/media_rw/extsdcard/temp
+	cat CPLOG
+	cat CPLOG >> mount_unmount_eMMCandSD_stress.log
 fi
