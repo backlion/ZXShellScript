90a28b6551196155d7b8da9b9dec534794700c41
lin199231 <lin199231@gmail.com>
2015-01-24 14:35:36 +0800

    [TEST]test.sh
    different_storage_performance_test的一些基础语句的测试

--- test.sh
+++ test.sh
@@ -6,4 +6,14 @@ case $1 in
 	5 ) echo 5;; 
 	6 ) echo 7;; 
 	* ) echo exit ;; 
-esac 
+esac
+
+for i in $(seq 10);do echo $i;done
+size=`ls -l CPFILE | awk '{total+=$4} END {print total}'`
+echo "    1m13.41s real     0m1.48s user     0m0.89s system" | awk 'BEGIN {FS="m|s|\\."}{print $1*60*1000+$2*1000+$3}'
+
+echo "    1m13.41s real     0m1.48s user     0m0.89s system" | awk 'BEGIN {FS="m|s|\\."}{print $1"_"$2"_"$3"_"$4}'
+
+cp_time=`(time cp -rf CPFILE /data) 2>&1 | tee -a 1.txt | awk 'BEGIN {FS="m|\\."}{print $1*60+$2}'`
+
+expr $(expr $size \* 1000) / 73041
