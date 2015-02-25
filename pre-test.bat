721f8fb4d945b3cb43afe86451e0d8e28514b047
lin199231 <lin199231@gmail.com>
2015-02-25 14:50:08 +0800

    [TEST]etc
    试验脚本

--- pre-test.bat
+++ pre-test.bat
@@ -1,4 +1,3 @@
-cd C:\Users\issaclin\Documents\GitHub\ZXShellScript
 cmd /c adb push C:\Users\issaclin\Desktop\QT_Audio /sdcard/Music
 cmd /c adb push "D:\other_video\CHP@CHD_Tsai Chin.001_1920x1080_23Mbps_30fps.avi" /sdcard/Movies
 cmd /c adb push D:\other_video\VC1_KYOTO_1920x1080_10Mbps_30fps.wmv /sdcard/Movies
