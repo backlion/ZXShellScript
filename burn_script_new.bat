b322bc8c6af3d2808266c97c9530bf01b980970f
lin199231 <lin199231@gmail.com>
2015-02-27 18:33:37 +0800

    [UPDATE]burn_script_new.bat
    增加通过参数来对A1,A2的devicetree的进行选择

--- burn_script_new.bat
+++ burn_script_new.bat
@@ -16,17 +16,45 @@ IF %ERRORLEVEL% == 0 echo Flash userdata image succeed
 IF NOT %ERRORLEVEL% == 0 goto 1
 
 echo Start flash devicetree to emmc
-	if "%1"=="" goto dsi
-	if "%1"=="dsi" goto dsi
-	if "%1"=="hdmi" goto hdmi
-	:dsi
-	echo flash DSI devicetree
-	fastboot.exe flash devicetree %~dp0/image/elite2000-fpga.dtb
-	goto continue
-	:hdmi
-	echo flash HDMI devicetree
-	fastboot.exe flash devicetree %~dp0/image/elite2000-fpga-mhl.dtb
-	goto continue
+	if "%2"=="" goto orginal
+	if "%2"=="A1" goto A1
+	if "%2"=="A2" goto A2
+	:orginal
+		if "%1"=="" goto dsi
+		if "%1"=="dsi" goto dsi
+		if "%1"=="hdmi" goto hdmi
+		:dsi
+		echo flash orginal DSI devicetree
+		fastboot.exe flash devicetree %~dp0/image/elite2000-fpga.dtb
+		goto continue
+		:hdmi
+		echo flash orginal HDMI devicetree
+		fastboot.exe flash devicetree %~dp0/image/elite2000-fpga-mhl.dtb
+		goto continue
+	:A1
+		if "%1"=="" goto A1_dsi
+		if "%1"=="dsi" goto A1_dsi
+		if "%1"=="hdmi" goto A1_hdmi
+		:A1_dsi
+		echo flash A1 DSI devicetree
+		fastboot.exe flash devicetree %~dp0/image/elite2000-EVM-A1-dsi.dtb
+		goto continue
+		:A1_hdmi
+		echo flash A1 HDMI devicetree
+		fastboot.exe flash devicetree %~dp0/image/elite2000-EVM-A1-hdmi.dtb
+		goto continue
+	:A2
+		if "%1"=="" goto A2_dsi
+		if "%1"=="dsi" goto A2_dsi
+		if "%1"=="hdmi" goto A2_hdmi
+		:A2_dsi
+		echo flash A2 DSI devicetree
+		fastboot.exe flash devicetree %~dp0/image/elite2000-EVM-A2-dsi.dtb
+		goto continue
+		:A2_hdmi
+		echo flash A2 HDMI devicetree
+		fastboot.exe flash devicetree %~dp0/image/elite2000-EVM-A2-hdmi.dtb
+		goto continue
 	:continue
 IF %ERRORLEVEL% == 0 echo Flash devicetree image succeed
 IF NOT %ERRORLEVEL% == 0 goto 1
