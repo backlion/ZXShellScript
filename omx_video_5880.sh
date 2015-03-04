3f36345ef76878632084cabfd5635bfd112b0be0
lin199231 <lin199231@gmail.com>
2015-03-04 12:11:05 +0800

    [UPDATE]omx_video_5880.sh
    1.由于5880运行video stress的特殊性，脚本使用相对路径，所以去除脚本的路径显示
    2.增加脚本的log保存

--- omx_video_5880.sh
+++ omx_video_5880.sh
@@ -4,6 +4,18 @@ if test -z $1
 then
 	echo "You must input a video"
 else
+	if test -e omx_video_5880.log
+	then
+		rm omx_video_5880.log
+	fi
+	echo "omx_video_5880 version: 1.1"
+	echo "omx_video_5880 version: 1.1"  >> omx_video_5880.log
+	if test -z $2
+	then
+		echo "set script sleep time 600 seconds"  >> omx_video_5880.log
+	else
+		echo "set script sleep time $2 seconds"  >> omx_video_5880.log
+	fi
 	#video=$1
 	LOOP=0
 	while true
@@ -11,7 +23,8 @@ else
 		let LOOP++
 		#echo "play $path/$video $LOOP times"
 		#am start -n com.android.gallery3d/.app.MovieActivity -d "$path/$video"
-		echo "play $path/$1 $LOOP times"
+		echo "play $1 $LOOP times"
+		echo "play $1 $LOOP times" >> omx_video_5880.log
 		am start -n com.android.gallery3d/.app.MovieActivity -d "/sdcard/Movies/$1"
 		if test -z $2
 		then
