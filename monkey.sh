d8a1ba170602860fc47d54ce26d37a909da018f2
= <=>
2015-03-11 09:23:08 +0800

    [UPDATE]monkey.sh
    更新monkey命令

--- monkey.sh
+++ monkey.sh
@@ -1,3 +1,3 @@
 logcat -v time &
 logcat -v time -b events > events_log &
-monkey --pkg-blacklist-file /sdcard/mblacklist.txt --throttle 1000 10000000 &
+monkey -v --pkg-blacklist-file /sdcard/mblacklist.txt --throttle 1000 10000000 &
