376caa5a85c17d159b4f88e1655af0d6ea07a11c
= <=>
2015-03-18 15:53:54 +0800

    [UPDATE]monkey.sh
    0 配置monkey环境
    1 run no_ignore monkey
    2 run ignore monkey
    修复相对路径的问题
    修复fix语句结尾符号的问题

--- monkey.sh
+++ monkey.sh
@@ -1,3 +1,33 @@
-logcat -v time &
-logcat -v time -b events > events_log &
-monkey -v --pkg-blacklist-file /sdcard/mblacklist.txt --throttle 1000 10000000 &
+if test -z $1
+then
+	echo "args list:"
+	echo "0 setup monkey environment"
+	echo "1 monkey -v --pkg-blacklist-file /sdcard/mblacklist.txt --throttle 1000 10000000 &"
+	echo "2 monkey -v --ignore-crashes --ignore-timeouts --ignore-native-crashes --pkg-blacklist-file mblacklist.txt --throttle 1000 1000000 &"
+	echo "h show this message"
+else
+	case $1 in 
+		0 ) mount -o remount,rw /system
+			rm /system/app/Gallery2/Gallery2.apk
+			rm /system/app/QuickSearchBox/QuickSearchBox.apk
+			rm /system/priv-app/Settings/Settings.apk
+			cp Gallery2.apk /system/app/Gallery2/
+			cp QuickSearchBox.apk /system/app/QuickSearchBox/
+			cp Settings.apk /system/priv-app/Settings/
+			chmod 644 /system/app/Gallery2/Gallery2.apk
+			chmod 644 /system/app/QuickSearchBox/QuickSearchBox.apk
+			chmod 644 /system/priv-app/Settings/Settings.apk
+			reboot;;
+		1 ) monkey -v --pkg-blacklist-file mblacklist.txt --throttle 1000 10000000 &
+			logcat -v time &
+			logcat -v time -b events > events_log &;;
+		2 ) monkey -v --ignore-crashes --ignore-timeouts --ignore-native-crashes --pkg-blacklist-file mblacklist.txt --throttle 1000 1000000 &
+			logcat -v time &
+			logcat -v time -b events > events_log &;;
+		* ) echo "args list:"
+			echo "0 setup monkey environment"
+			echo "1 monkey -v --pkg-blacklist-file /sdcard/mblacklist.txt --throttle 1000 10000000 &"
+			echo "2 monkey -v --ignore-crashes --ignore-timeouts --ignore-native-crashes --pkg-blacklist-file mblacklist.txt --throttle 1000 1000000 &"
+			echo "h show this message";;
+	esac
+fi
