1c3568d3f3bf46550f421d7b09076e7f56b43892
= <=>
2015-04-01 11:17:06 +0800

    [UPDATE]mount_unmount_eMMCandSD_stress
    改为stress以及增加对于mount/umount的判断

--- mount_unmount_eMMCandSD_stress.sh
+++ mount_unmount_eMMCandSD_stress.sh
@@ -1,34 +1,55 @@
 #Author：Issac Lin
-#Ext: 7536
-#version:1.1
+#Ext: 756706
+#version:1.2
 #mount_unmount_eMMCandSD_stress
 #log_location:./mount_unmount_eMMCandSD_stress.log
 #
-echo "mount_unmount_eMMCandSD_stress version: 1.1"
-if test -z $1
+echo "mount_unmount_eMMCandSD_stress version: 1.2"
+if test -z $(ls /mnt/media_rw/extsdcard)
 then
-	echo "You must input the mount and unmount times"
+	echo "undetected SD card"
+	echo "make sure that you have plug the SD card"
 else
-	rm mount_unmount_eMMCandSD_stress.log
+	if test -e mount_unmount_eMMCandSD_stress.log
+	then
+		rm mount_unmount_eMMCandSD_stress.log
+	fi
 	LOOP=1
-#	mkdir /data/fat
+	#mkdir /data/fat
 	mount -o remount,rw /
-	mkdir /temp
-	umount /mnt/media_rw/extsdcard
+	#mkdir /temp
+	#install busybox
+		mount -o remount,rw /system
+		cp busybox /system/xbin
+		chmod 777 /system/xbin/busybox
+		/system/xbin/busybox --install /system/xbin
+		echo "finish install busybox"
+		echo "finish install busybox" >> different_storage_copy-compare_test.log
+	mkdir /mnt/media_rw/extsdcard/temp
 	date >> mount_unmount_eMMCandSD_stress.log
-	while test LOOP -le $1
+	while true
 	do
-		mount -t vfat /dev/block/mmcblk0p1  /mnt/media_rw/extsdcard
-		echo "mount $LOOP times"
-		echo "mount $LOOP times" >> mount_unmount_eMMCandSD_stress.log
 		umount /mnt/media_rw/extsdcard
 		echo "unmount $LOOP times"
 		echo "unmount $LOOP times" >> mount_unmount_eMMCandSD_stress.log
+		if test -z $(ls /mnt/media_rw/extsdcard)
+		then
+			echo "success to unmount SD card"
+			echo "success to unmount SD card" >> mount_unmount_eMMCandSD_stress.log
+		fi
+		mount -t vfat /dev/block/mmcblk0p1  /mnt/media_rw/extsdcard
+		echo "mount $LOOP times"
+		echo "mount $LOOP times" >> mount_unmount_eMMCandSD_stress.log
+		if test ! -z $(ls /mnt/media_rw/extsdcard)
+		then
+			echo "success to mount SD card"
+			echo "success to mount SD card" >> mount_unmount_eMMCandSD_stress.log
+		fi
+		sh via_cpcmp test /mnt/media_rw/extsdcard/temp
+		echo "copy&compare $LOOP times"
+		echo "copy&compare $LOOP times" >> mount_unmount_eMMCandSD_stress.log
+		cat CPLOG
+		cat CPLOG >> mount_unmount_eMMCandSD_stress.log
 		let LOOP++
 	done
-	mount -t vfat /dev/block/mmcblk0p1  /mnt/media_rw/extsdcard
-	mkdir /mnt/media_rw/extsdcard/temp
-	sh via_cpcmp test /mnt/media_rw/extsdcard/temp
-	cat CPLOG
-	cat CPLOG >> mount_unmount_eMMCandSD_stress.log
 fi
