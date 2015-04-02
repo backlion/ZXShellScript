ca0e4d15fea8f14b97e24ee5d208d5cb93d63018
= <=>
2015-04-02 18:46:26 +0800

    [TEST]
    CRT脚本运行测试

--- test.sh
+++ test.sh
@@ -1,1 +1,21 @@
-echo hello
+input swipe 500 1000 500 500
+sleep 1
+input tap 1800 1000
+sleep 1
+input tap 1750 567
+sleep 1
+input tap 1800 1000
+sleep 1
+am start -n com.android.settings/.DisplaySettings
+sleep 1
+input tap 500 600
+sleep 1
+input tap 600 835
+sleep 1
+am start -n com.android.settings/.SecuritySettings
+sleep 1
+input tap 500 300
+sleep 1
+input tap 500 200
+sleep 1
+input keyevent 3
